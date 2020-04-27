6dc66c5edf721153d4f398f5747a9faddcb956e2
asherhoffman1120 <asher.hoffman1120@gmail.com>
2020-04-27 12:44:52 +0530

    refinements in delivery address layout !

--- app/src/main/java/org/nearbyshops/enduserappnew/Preferences/PrefGeneral.java
+++ app/src/main/java/org/nearbyshops/enduserappnew/Preferences/PrefGeneral.java
@@ -3,6 +3,7 @@ package org.nearbyshops.enduserappnew.Preferences;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import androidx.fragment.app.Fragment;
 
 import org.nearbyshops.enduserappnew.MyApplication;
 import org.nearbyshops.enduserappnew.R;
